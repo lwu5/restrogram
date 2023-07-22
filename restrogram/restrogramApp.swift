5f0f70f77af51d630af908d93c862335489416d0
Liuhao Wu <liuhaowu@uchicago.edu>
2023-07-21 17:19:19 -1000

    Initial Commit

---
+++ restrogram/restrogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  restrogramApp.swift
+//  restrogram
+//
+//  Created by LEONARD on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct restrogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
